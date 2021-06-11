021d4e1a3635c4de0dde13d720c31eccac175458
qycl <qy50224@163.com>
2021-06-11 06:41:25 -0700

    fix bug

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -94,4 +95,32 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+	int mask;
+	if (argint(0, &mask) < 0)
+		return -1;
+	myproc()->trace_mask = mask;
+	return 0;
+
+}
+
+uint64
+sys_sysinfo(void)
+{
+	struct sysinfo s; 
+	uint64 dest;
+	struct proc* p = myproc();
+	if (argaddr(0, &dest) < 0) {
+		return -1;
+	}	
+	s.freemem = get_total_free();
+	s.nproc = get_num_proc();
+	if (copyout(p->pagetable, dest, (char *)&s, sizeof s) < 0) {
+		return -1;
+	}
+	return 0;
 }
